23a96b163a97b45e6f7b9aa6d63502a746c8ba5d
Galym Kulyn <galimkulin@icloud.com>
2019-10-10 00:10:16 +0600

    refactor: set right component type

--- examples/text_area_bot.py
+++ examples/text_area_bot.py
@@ -1,47 +1,55 @@
 import logging
 from pyAitu import executor, Bot, Dispatcher
-from pyAitu.models import Message, Item, Options, Form, Header, FormClosed, TextArea, ValidationRule
+from pyAitu.models import Message, Options, Form, Header, FormClosed, \
+    FormSubmitted, TextArea, Submit, FormAction, ValidationRule
 
-API_TOKEN = '7e26eb35-2b45-46d5-8e1b-f79a4bfcccc4'
+API_TOKEN = 'YOUR_API_TOKEN'
 
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+dispatcher = Dispatcher(bot)
 
 logging.basicConfig(level=logging.INFO)
 
 
-@dp.message_handler()
-async def send_ui(message: Message):
-
-    # Header setting
-    text_input_header = Header(
-        _type="title",
-        title="Input text component",
-        options=Options(
-            closeable=True
-        )
-    )
-
-    # Content setting
+def make_form() -> Form:
+    form_id = "text_area_form_id"
+    header = Header(_type="title", title="Text area component", options=Options(closeable=True))
     text_area = TextArea(
-        content_id="text_area_content_id",
-        content_type="input",
-        title="Cost",
+        content_id="text_area_id",
+        content_type="text_area",
+        title="Text Area",
         text="",
-        placeholder="Enter your price"
+        placeholder="Enter your text here",
+        validations_rules=[ValidationRule(type="required", value="true", error="You must fill this area")]
     )
+    submit = Submit(
+        content_id="submit_id",
+        content_type="submit",
+        title="Send",
+        form_action=FormAction(action="submit_form", data_template="{"+form_id+".text_area_id}")
+    )
+    content = [text_area, submit]
+    return Form(_id=form_id, header=header, content=content)
 
-    # Form setting
-    form = Form(_id="form_id", header=text_input_header, content=text_area)
 
-    # Form sending
+@dispatcher.message_handler()
+async def handle(message: Message):
+    form = make_form()
     await bot.send_form(message.chat.id, form=form)
 
 
-@dp.form_closed_handler()
-async def get_form_closed(fc: FormClosed):
-    await bot.send_message(fc.chat.id, "form is closed")
+@dispatcher.form_submitted_handler()
+async def handle_submission(submitted_form: FormSubmitted):
+    await bot.send_message(
+        submitted_form.chat.id,
+        f"Oh, it seems I have received text from you, look:\n\"{submitted_form.metadata}\""
+    )
+
+
+@dispatcher.form_closed_handler()
+async def handle_form_closing(closed_form: FormClosed):
+    await bot.send_message(closed_form.chat.id, "Form is closed")
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dispatcher)
