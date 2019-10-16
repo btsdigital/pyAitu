9bb0f2527401cc4cc5b3de73a853db0a9327f52d
Galym Kulyn <galimkulin@icloud.com>
2019-10-16 23:04:15 +0600

    feat: dictionary none purifier

--- pyAitu/utils/dictionary_extractor.py
+++ pyAitu/utils/dictionary_extractor.py
@@ -1,13 +1,33 @@
-from typing import Dict, List, Optional
+from typing import Dict, Optional, Any
+import copy
+
+# Type aliases
+Object = Any
+String = str
 
 
-def extract_property_if_exist_from(object: any, property_name: str) -> Optional[any]:
+def property_of_object_if_exist(object: Optional[Object], property_name: String) -> Optional[Any]:
+    if object is None:
+        return None
     if hasattr(object, "__getattribute__") and hasattr(object, property_name):
         return object.__getattribute__(property_name)
     else:
         return None
 
 
-def extract_dictionary_if_exist_from(object: any) -> Optional[Dict[str, any]]:
-    dictionary = extract_property_if_exist_from(object, property_name="__dict__")
-    return None if not dictionary else dictionary
+def dictionary_of_object_if_exist(object: Optional[Object]) -> Optional[Dict[Any, Any]]:
+    return property_of_object_if_exist(object, property_name="__dict__")
+
+
+# Shallow purification from None on arbitrary Python Dictionaries. Returns purified dictionary.
+def dictionary_purified_from_none(dictionary: Dict[Any, Any]) -> Dict[Any, Any]:
+    return dict(filter(lambda item: item[1], dictionary.items()))
+
+
+# Shallow purification from None on arbitrary Python Objects. Returns purified deepcopy of object.
+def object_purified_from_none(object: Object) -> Object:
+    new_object = copy.deepcopy(object)
+    object_dictionary = dictionary_of_object_if_exist(new_object)
+    if object_dictionary is not None:
+        new_object.__dict__ = dictionary_purified_from_none(object_dictionary)
+    return new_object
